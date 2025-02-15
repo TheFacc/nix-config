0d8ff176e1a3a18b67b374ac4b5686b01f1dc93d
TheFacc <imthefacc@gmail.com>
2025-02-15 17:40:41 +0100

    vscode cleanup

--- home/facc/common/optional/dev/vscode.nix
+++ home/facc/common/optional/dev/vscode.nix
@@ -1,25 +1,34 @@
 # { system ? builtins.currentSystem, inputs, config, pkgs, ... }:
-# { outputs, ... }:
+{ pkgs, inputs, ... }:
 # should we include this? https://github.com/NixOS/nixpkgs/commit/b2eb5f62a7fd94ab58acafec9f64e54f97c508a6
 let
   # system = outputs.system; #TODO inherit system somehow
   system = "x86_64-linux";
-  VSCextensions = #lib.genAttrs ss (system:
-    (import (builtins.fetchGit {
-      url = "https://github.com/nix-community/nix-vscode-extensions";
-      ref = "refs/heads/master";
-      #rev = "cc9b4bc44e8518285a6499959dcf60dfb63dd807"; # 03-jan-2024
-      #rev = "9f6b2b21066043d8420dc67798de0a5a5cd2318e"; # 17-feb-2024
-      rev = "ebf9b6e2c3252dfcd375a06b69723e0065091568"; # 19-mar-2024 #TODO this looks awful now right uhm think about it
-    })).extensions.${system};
-  extensionsList = with VSCextensions.vscode-marketplace; [
+  extPk = inputs.nix-vscode-extensions.extensions.${system};
+  extensionsList = with extPk.open-vsx; [
+        # https://raw.githubusercontent.com/nix-community/nix-vscode-extensions/master/data/cache/open-vsx-latest.json
           # Theme
           monokai.theme-monokai-pro-vscode # -> Filter Spectrum
+
+          # Tools
           gruntfuggly.todo-tree
+          eamodio.gitlens
 
           # Nix
           bbenoist.nix
-          
+
+          # Nuxt
+          nuxtr.nuxt-vscode-extentions
+          nuxtr.nuxtr-vscode
+          nuxt.mdc
+          vue.volar
+          # webdev
+          pflannery.vscode-versionlens
+          dbaeumer.vscode-eslint
+          esbenp.prettier-vscode
+
+      ] ++ (with extPk.vscode-marketplace; [
+        # https://raw.githubusercontent.com/nix-community/nix-vscode-extensions/master/data/cache/vscode-marketplace-latest.json
           # C++
           # - Microsoft:
 #          ms-vscode.cpptools
@@ -27,41 +36,25 @@ let
 #          ms-vscode.cpptools-themes
         # ms-vscode.makefile-tools # broken?? cant switch
           # - Clangd:
-          llvm-vs-code-extensions.vscode-clangd # requires pkgs.llvmPackages_17.clang-unwrapped
+          # llvm-vs-code-extensions.vscode-clangd # requires pkgs.llvmPackages_17.clang-unwrapped
           # - CMake:
-          twxs.cmake
+          # twxs.cmake
           # ms-vscode.cmake-tools # derivation fails to build with v1.86.2 as of 17-feb-24
           
           # MATLAB
-          mathworks.language-matlab
+          # mathworks.language-matlab
           
           # Github
           github.copilot
-#          github.copilot-chat #-> forcing old version below
+          # github.copilot-chat #-> forcing 'release' version below, instead of 'latest' which is too new and always incompatible with the current vscode version
 #          github.copilot-labs -> discontinued
-          eamodio.gitlens
           #sourcegraph.cody-ai
           
-          # Nuxt
-          nuxtr.nuxt-vscode-extentions
-          nuxtr.nuxtr-vscode
-          nuxt.mdc
-          vue.volar
           antfu.goto-alias
-          # webdev
-          pflannery.vscode-versionlens
-          dbaeumer.vscode-eslint
-          esbenp.prettier-vscode
-  ];
-
-  # force extension version:
-  extensionsStable =# lib.genAttrs outputs.systems (system:
-    (import (builtins.fetchGit {
-      url = "https://github.com/nix-community/nix-vscode-extensions";
-      ref = "refs/heads/master";
-      rev = "0865dd098cd9dc4fa80157d8b19f1c3a242de238"; # 29-feb-2024
-    })).extensions.${system};
-  extCopilotChat = with extensionsStable.vscode-marketplace; [ github.copilot-chat ];
+      ]) ++ (with extPk.vscode-marketplace-release; [
+        # https://raw.githubusercontent.com/nix-community/nix-vscode-extensions/master/data/cache/vscode-marketplace-release.json
+          github.copilot-chat
+      ]);
 
 in
 
@@ -70,6 +63,21 @@ in
     programs.vscode = {
       enable = true;
       # package = pkgs.vscodium;
-      extensions = extensionsList ++ extCopilotChat;
+      mutableExtensionsDir = false;
+      enableUpdateCheck = false;
+      enableExtensionUpdateCheck = false;
+      extensions = extensionsList;
+      userSettings = {
+        # "editor.fontFamily" = "'FiraCode Nerd Font', 'FiraCode Nerd Font Mono', 'monospace', monospace";
+        "window.titleBarStyle" = "custom";
+        "workbench.colorTheme" = "Monokai Pro (Filter Spectrum)";
+        "github.copilot.editor.enableAutoCompletions" = true;
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nixd";
+        "nix.formatterPath" = "nixfmt";
+        "formatting" = {
+          "command" = "nixfmt";
+        };
+      };
     };
 }
